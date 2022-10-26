e2a5d0172a3c6429f0caf330f91c15fcafa04c8b
Chris Kaltwasser <chris@chrisk.com>
2022-10-26 16:56:17 -0400

    changed value to object.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export const UserContext = React.createContext();
 
 export default function App() {
   return (
-    <UserContext.Provider value="Reed">
+    <UserContext.Provider value={ { "first": "Theo", "last": "Kaltwasser" } } >
       <User />
     </UserContext.Provider>
   )
@@ -13,5 +13,5 @@ export default function App() {
 function User() {
   const value = React.useContext(UserContext);  
     
-  return <h1>{value}</h1>;
+  return <h1>{value.first} {value.last}</h1>;
 }
